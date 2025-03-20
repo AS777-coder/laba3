029adecf3a3136bda4397bce1ede9452a6b8ade8
AS777-coder <artemyhedgehog@mail.ru>
2025-03-20 11:29:57 +0300

    Created new files

---
+++ calculator.py
@@ -0,0 +1,14 @@
+import functools
+import operator
+
+def summ(*args):
+    return functools.reduce(operator.add, args)
+
+def mull(*args):
+    return functools.reduce(operator.mul, args)
+
+def fac(x: int):
+    return functools.reduce(operator.mul, [i for i in range(1, x+1)])
+
+def poww(a: int, b: int):
+    return a**b
